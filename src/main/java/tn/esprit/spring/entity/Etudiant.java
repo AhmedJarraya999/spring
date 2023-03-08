adde92ad7827d366ab267b0087f1c294d9f20937
AhmedJarraya999 <96777472+AhmedJarraya999@users.noreply.github.com>
2023-03-08 14:54:56 +0100

    first commit

---
+++ src/main/java/tn/esprit/spring/entity/Etudiant.java
@@ -0,0 +1,44 @@
+package tn.esprit.spring.entity;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table( name ="Etudiant")
+public class Etudiant implements Serializable {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="idEtudiant")
+    private Long idEtudiant; // Clé primaire
+    private String prenomE;
+    private String nomE;
+
+// Constructeur et accesseurs (getters) et mutateurs (setters)
+
+
+    public Etudiant() {
+    }
+
+    public String getNomE() {
+        return nomE;
+    }
+
+    public void setNomE(String nomE) {
+        this.nomE = nomE;
+    }
+
+    public String getPrenomE() {
+        return prenomE;
+    }
+
+    public void setPrenomE(String prenomE) {
+        this.prenomE = prenomE;
+    }
+
+    public Long getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(Long idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+}
